a20ba760e4655bed9940da741154d17c0cc711e0
thiagop-usp <thiagop@usp.br>
2019-03-08 00:41:20 -0300

    all urls are fetched all okay just gotta start the scrapin now

--- scraper.py
+++ scraper.py
@@ -1,16 +1,34 @@
-import requests
+from requests_html import HTMLSession
 from bs4 import BeautifulSoup as bs
-default_url = 'https://www.guiamais.com.br/encontre?searchbox=true&'
+
+session = HTMLSession()
+default_url = 'https://www.guiamais.com.br/encontre?searchbox=true'
+default_modifier = '&page='
 
 class Scraper:
-    different_pages = []
 
-    def __init__(self, url=default_url, what, where):
+
+    def __init__(self, what, where, url=default_url, modifier=default_modifier):
         self.url = url
         self.what = what
         self.where = where
+        self.modifier = default_modifier
+        self.different_pages = []
+
+        r = session.get(f'{self.url}&what={self.what}&where={self.where}{self.modifier}9999')
+        xpath = r.html.xpath('//*[@id="list"]/nav/a')[-1]
+        final_url = min(xpath.links)
+        self.max_pages = int(final_url.split('&page=')[-1])
 
     def get_html(self):
-        query_url = f'{self.url}what={self.what}&where={self.where}&page='
-        for page in range(1, max_pages+1):
-            different_pages.append(query_url+str(page))
+        query_url = f'{self.url}what={self.what}&where={self.where}{self.modifier}'
+        for page in range(1, self.max_pages+1):
+            self.different_pages.append(query_url+str(page))
+            print(query_url+str(page))
+
+    #def scrape_all(self):
+
+
+if __name__ == '__main__':
+    bot = Scraper('Restaurante', 'Campinas')
+    bot.get_html()
